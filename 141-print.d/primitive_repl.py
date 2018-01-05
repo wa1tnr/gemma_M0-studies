d8c64574356ff450184cd392a30b6c9836ca02c5
wa1tnr <wa1tnr@users.noreply.github.com>
2018-01-05 21:47:08 +0000

    modified:   primitive_repl.py Revision

--- 141-print.d/primitive_repl.py
+++ 141-print.d/primitive_repl.py
@@ -7,58 +7,52 @@
 
 # rollback -- establishing the history of this file (all 36 hours of it).
 
-import builtins
-import time
-counter = 4
-
-# ---------------------------
-print("turna sevixale bce78")
-# ---------------------------
-
-def ESC_color():
-    print("\x1b\x5b0;1;", end='',)
-
-def neutral():
-    ESC_color()
-    print("0m", end='',)
-
-# -----------------fuzzy
+import builtins ; import time
 
 def prompt():
-    print(" ok", end='\n',)
-
-    # print(" ok", end='\n',)
+    print(" ok  ", end='',)
 
 def get_things():
-    # tib=input("prompted for tib content:")
-    # tib=raw_input()
-    tib=input()
-    tab=int(tib)
-    tab=tab - 11
-    print(tab)
-    prompt()
+    global tib ; tib=input(); # terminal input buffer
+    global tab ; tab=''
+    try:
+        tab='' ; tab = int(tib)
+
+    except ValueError:
+        pass
+
+    if (isinstance(tab, int)):
+        print("tab was an int. ", end='')
+
+    prompt() ; return tab, tib
 
 def signon():
-    neutral(); print("OK. ", end='')
-    time.sleep(0.28)
+    print("ooask. ", end='')
+    print("OK. ", end='')
 
 # ----------------              -----------------
 
 def looping():
+
     while True:
-        global counter
         get_things()
-        counter = counter + 1
-        time.sleep(0.28)
+        if (isinstance(tab, int)):
+            print("printing integer tab: ", end='')
+            print(tab)
+            print("less 7: ", end='')
+            print(tab -7)
 
+        if (isinstance(tab, str)):
+            m = 1 ; # noop # print("ERROR - detect and ignore.")
+
+        if (isinstance(tib, str)):
+            if (len(tib) > 0):
+                print("printing the str, tib, the terminal input buffer: ")
+                print(tib)
+
+        time.sleep(0.128)
 # - - - - - - - - - - - - - - - - - - - -
-# Let's get going..
-
 signon()
-# looping()
-
-
-# end of program.  See sample run, below:
-
-# --- end of sample run ---
+# looping()  # run this in the REPL:
+#     'import main;main.looping()'
 # END.
